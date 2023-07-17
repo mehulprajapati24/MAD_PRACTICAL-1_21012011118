f0be92221643ffb05ded50eb3e21606278f68cd6
student <student@gnu.ac.in>
2023-07-17 10:40:44 +0530

    practical_1.1 completed

--- src/main/kotlin/practical_1.1.kt
+++ src/main/kotlin/practical_1.1.kt
@@ -1,4 +1,14 @@
 fun main()
 {
-    println("Hello World!")
+    var i:Int=5
+    var d:Double=5.666
+    var f:Float=5.6F
+    var l:Long=56666666L
+    var s:Short=5000
+    var b:Byte=55
+    var c:Char='m'
+    var bl:Boolean=true
+    var str:String="Mehul"
+
+    println("Integer: $i, Double: $d, Float: $f, Long: $l, Short: $s, Byte: $b, Char: $c, Boolean: $bl, String: $str")
 }
