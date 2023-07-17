2b15ba8b729b209619159ff8fd4a039feb630a5f
user <mehulprajapati1661@gmail.com>
2023-07-17 22:04:11 +0530

    practical_1.7 half completed

---
+++ src/main/kotlin/practical_1.7.kt
@@ -0,0 +1,29 @@
+fun main()
+{
+    print("Enter number to find factorial : ")
+    var s:String? = readLine()
+    var num:Int = s?.toIntOrNull()?:0
+
+    if(num<0)
+    {
+        println("Please enter valid number!!")
+    }
+    else
+    {
+        val result = factorial(num)
+        println("Factorial of $num = $result")
+    }
+}
+
+fun factorial(num:Int):Int {
+
+    if(num==0 || num==1)
+    {
+        return 1
+    }
+
+    else
+    {
+        return num * factorial(num-1)
+    }
+}
