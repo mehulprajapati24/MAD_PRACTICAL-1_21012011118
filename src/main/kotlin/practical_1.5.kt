988b68b6f26c6291e1417937afdd10ddf921c957
student <student@gnu.ac.in>
2023-07-18 09:28:25 +0530

    practical_1.5 updated

--- src/main/kotlin/practical_1.5.kt
+++ src/main/kotlin/practical_1.5.kt
@@ -5,8 +5,29 @@ fun main()
 //    var no: Int = s?.toIntOrNull() ?: 0
 
     var no:Int = readLine()!!.toInt()
-    val month = when(no)
-    {
+//    val month = when(no)
+//    {
+//        1 -> "January"
+//        2 -> "February"
+//        3 -> "March"
+//        4 -> "April"
+//        5 -> "May"
+//        6 -> "June"
+//        7 -> "July"
+//        8 -> "August"
+//        9 -> "September"
+//        10 -> "October"
+//        11 -> "November"
+//        12 -> "December"
+//        else -> "Invalid month"
+//    }
+    val month = displayMonth(no)
+
+    println("Month name: $month")
+}
+
+fun displayMonth(obj: Any):String =
+    when(obj){
         1 -> "January"
         2 -> "February"
         3 -> "March"
@@ -21,6 +42,3 @@ fun main()
         12 -> "December"
         else -> "Invalid month"
     }
-
-    println("Month name: $month")
-}
