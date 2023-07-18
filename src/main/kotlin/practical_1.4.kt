3dcfee83f2ea5b0cdd10e0ee0ad5cdbd8cf254d4
student <student@gnu.ac.in>
2023-07-18 09:17:15 +0530

    practical_1.4 updated

--- src/main/kotlin/practical_1.4.kt
+++ src/main/kotlin/practical_1.4.kt
@@ -1,7 +1,8 @@
 fun main() {
     print("Enter number to check number is odd or even : ")
-    var s: String? = readLine()
-    var no: Int = s?.toInt() ?: 0
+//    var s: String? = readLine()
+//    var no: Int = s?.toInt() ?: 0
+    var no: Int = readLine()!!.toInt()
 
     println(if(no%2==0) "$no is even number" else "$no is odd number")
 //  var result = if(no%2==0) "$no is even number" else "$no is odd number"
