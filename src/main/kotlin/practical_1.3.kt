78d4d1be44541c1f61993cd0015a69e39d56a984
student <student@gnu.ac.in>
2023-07-18 09:43:14 +0530

    practical_1.3 updated

--- src/main/kotlin/practical_1.3.kt
+++ src/main/kotlin/practical_1.3.kt
@@ -7,8 +7,27 @@ fun main()
     print("Enter your name: ")
     var name:String? = readLine()
 
+    print("Enter your branch: ")
+    var branch:String? = readLine()
+
+    print("Enter your class: ")
+    var cls:String? = readLine()
+
+    print("Enter your batch: ")
+    var batch:String? = readLine()
+
+    print("Enter your college name: ")
+    var college:String? = readLine()
+
+    print("Enter your university name: ")
+    var university:String? = readLine()
+
+    print("Enter your age : ")
+    var str:String? = readLine()
+    var age:Int = str?.toInt()?:0
 //    print("Enter your name: ")
 //    var name:String = readln()
 
-    println("YOUR ENROLLMENT NO: $enroll AND NAME: $name")
+    println("********************************")
+    println("Student's data: \nENROLLMENT NO: $enroll \nNAME: $name \nBRANCH: $branch \nCLASS: $cls \nBATCH: $batch \nCOLLEGE: $college \nUNIVERSITY: $university \nAGE: $age")
 }
